95c4ec0c797510df044f9f4499fda48752d0ed15
elijah-cyber7 <aandrew20044@gmail.com>
2024-08-08 14:18:13 -0500

    Added code to detect and track face in webcam

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
 import cv2
 import sys
 
-cascPath = sys.argv[1]
-faceCascade = cv2.CascadeClassifier(cascPath)
+cascadePath = sys.argv[1]
+faceCascade = cv2.CascadeClassifier(cascadePath)
 
 vid = cv2.VideoCapture(0)
 
 while(True):
-    ret, frame = vid.read()
+    r, frame = vid.read()
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    faces = faceCascade.detectMultiScale(
+    possibleFaces = faceCascade.detectMultiScale(
         gray,
         scaleFactor=1.1,
         minNeighbors=5,
         minSize=(30, 30)
     )
 
-    for (x, y, w, h) in faces:
+    for (x, y, w, h) in possibleFaces:
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
     cv2.imshow('video', frame)
